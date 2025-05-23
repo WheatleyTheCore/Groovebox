fa2f7f8d0157d83e1c4ffdd1f7007e2be354bffa
WheatleyTheCore <alex.colwell@icloud.com>
2025-05-22 22:31:43 -0400

    testing script now works correctly, and can calcualte teh desired metrics. now I just need to collect enough data!

--- hardware/DMI/DMI.ino
+++ hardware/DMI/DMI.ino
@@ -1,13 +1,14 @@
 #include <MovingAverageFilter.h>
+#include <ADCTouch.h>
 
-#define OFFSET_ENERGY_DELTA_THRESH 8
-#define OFFSET_SETTLE_TIME 60 // in ms
+#define OFFSET_ENERGY_DELTA_THRESH 0.3
+#define OFFSET_SETTLE_TIME 40 // in ms
 
 int rawPiezoInput;
 float filteredPizeoInput;
 float previousPiezoInput; // set really big just so whatever initial values are don't trigger an onset
 
-MovingAverageFilter pizeoFilter(3);
+MovingAverageFilter pizeoFilter(10);
 MovingAverageFilter pressureFilter(20);
 
 bool onsetFlag = true; // wait for it to settle so it doesn't send one on boot
@@ -21,8 +22,12 @@ float highpassValue = 0;
 int raw_avg_pressure;
 int filtered_avg_pressure;
 
+int capacitiveTouchRef, capacitiveTouchVal;
+bool isCapacitiveTouch = false;
+bool previousCapacitiveTouch = false;
+
 bool isThereOnset(float threshold) {
-      if (abs(filteredPizeoInput - previousPiezoInput) > threshold) {
+      if (filteredPizeoInput - previousPiezoInput > threshold) {
         return true;
       } else {
         return false;
@@ -38,33 +43,53 @@ void setup() {
   pinMode(A5, INPUT);
   Serial.begin(9600);
   EMA_S = analogRead(A0);     //set EMA S for t=1
+  capacitiveTouchRef = ADCTouch.read(A1);
   previousPiezoInput = EMA_S;
+  Serial.print("time (ms)");
+  Serial.print(',');
+  Serial.print("onset_detection");
+  Serial.print(',');
+  Serial.println("capacitive_detection");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   rawPiezoInput = analogRead(A0);  // read the input pin
   raw_avg_pressure = (analogRead(A2) + analogRead(A3) + analogRead(A4) + analogRead(A5)) / 4;
-  filtered_avg_pressure = pressureFilter.process(raw_avg_pressure) - 318; // evil magic number, quick band-aid to make pressure settle around 0
+  filtered_avg_pressure = pressureFilter.process(raw_avg_pressure) - 600; // evil magic number, quick band-aid to make pressure settle around 0
   EMA_S = (EMA_a*rawPiezoInput) + ((1-EMA_a)*EMA_S);
-  highpassValue = rawPiezoInput - EMA_S; 
+  highpassValue = constrain(rawPiezoInput - EMA_S, 0, 100); 
   filteredPizeoInput = pizeoFilter.process(highpassValue);
 
 //   onset stuff!
   detectedOnset = isThereOnset(OFFSET_ENERGY_DELTA_THRESH);
+
+  capacitiveTouchVal = ADCTouch.read(A1);
+  capacitiveTouchVal -= capacitiveTouchRef;
+  isCapacitiveTouch = capacitiveTouchVal > 40;
+
+  Serial.print(millis());
+  Serial.print(',');
   
-  
-  if (detectedOnset && !onsetFlag && filtered_avg_pressure < 20) {
+  if (detectedOnset && !onsetFlag && filtered_avg_pressure < 100) {
     onsetFlag = true;
     onsetTime = millis();
-    Serial.print(100);
+    Serial.print(true);
   } else {
-    Serial.print(0);
+    Serial.print(false);
   }
 
-  Serial.print(" ");
-  Serial.println(filtered_avg_pressure); 
+  
+  
+//  Serial.println(filteredPizeoInput);
+  Serial.print(',');
+  if (isCapacitiveTouch && !previousCapacitiveTouch) {
+    Serial.println(true); 
+  } else {
+    Serial.println(false); 
+  }
 
+  previousCapacitiveTouch = isCapacitiveTouch;
 
 
   if (!detectedOnset && onsetFlag) {
